d7b70b6b8426df899b1bcfdfa044a48fb40305bb
pFragga <84293693+pFragga@users.noreply.github.com>
2021-06-13 19:54:35 +0300

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -654,21 +654,84 @@ public class MainApp {
     
     //FileReader
     void readFile() throws IOException {
-        fileReader myFileReader = new fileReader();
-        FileReader Reader = new FileReader("expenseTypes");
-        BufferedReader bReader = new BufferedReader(Reader);
-        String line = null;
+        BufferedReader reader = null;
+        ExpenseType expenseType = null;
+        String line;
+        System.out.println("\n>>>>>>>>>>>>>> Adding Objects (ExpenseType) from File to List ...");
 
-        if (bReader != null) {
-            line = bReader.readLine();
-        }
-
-        if (bReader != null) {
-            bReader.close();
-        }
-
-        if (Reader != null) {
-            Reader.close();
+        try {
+            reader = new BufferedReader(new FileReader(new File("C:/Users/Petros/Documents/AUEB/javalab/ASKHSEIS/project2/expenseTypes")));
+            line = reader.readLine();
+            while (line != null) {
+                if (line.trim().equals("EXPENSE_TYPE_LIST")) {
+                    line = reader.readLine();
+                    if (line.trim().equals("{")) {
+                        line = reader.readLine();
+                        if (line.trim().equals("EXPENSE_TYPE")) {
+                            line = reader.readLine();
+                            if (line.trim().equals("{")) {
+                                line = reader.readLine();
+                                if (line.trim().startsWith("TYPE ")) {
+                                    if (line.trim().substring(5).trim().equals("1")) {
+                                        line = reader.readLine(); //maybe not needed
+                                        int id = 0;
+                                        if (line.trim().startsWith("ID ")) {
+                                            id = Integer.parseInt(line.trim().substring(3).trim());
+                                        }
+                                        line = reader.readLine(); //maybe not needed
+                                        String desc = null;
+                                        if (line.trim().startsWith("DESC ")) {
+                                            desc = line.trim().substring(5).trim();
+                                        }
+                                        line = reader.readLine(); //idk bro
+                                        double max = 0;
+                                        if (line.trim().startsWith("MAX_MONTHLY_EXPENSE ")) {
+                                            max = Double.parseDouble(line.trim().substring(20).trim());
+                                        }
+                                        line = reader.readLine(); //bababooey
+                                        double price = 0;
+                                        if (line.trim().startsWith("PRICE ")) {
+                                            price = Double.parseDouble(line.trim().substring(6).trim());
+                                        }
+                                        line = reader.readLine(); //drink piss and live
+                                        String unit = null;
+                                        if (line.trim().startsWith("UNIT_OF_MEASUREMENT ")) {
+                                            unit = line.trim().substring(20).trim();
+                                        }
+                                        expenseType = new ExpenseType1(id, desc, max, price, unit);
+                                        expenseTypes.add(expenseType);
+                                    } else if (line.trim().substring(5).trim().equals("2")) {
+                                        line = reader.readLine(); //maybe not needed
+                                        int id = 0;
+                                        if (line.trim().startsWith("ID ")) {
+                                            id = Integer.parseInt(line.trim().substring(3).trim());
+                                        }
+                                        line = reader.readLine(); //maybe not needed
+                                        String desc = null;
+                                        if (line.trim().startsWith("DESC ")) {
+                                            desc = line.trim().substring(5).trim();
+                                        }
+                                        line = reader.readLine(); //idk bro
+                                        double max = 0;
+                                        if (line.trim().startsWith("MAX_MONTHLY_EXPENSE ")) {
+                                            max = Double.parseDouble(line.trim().substring(20).trim());
+                                        }
+                                        line = reader.readLine(); //lesgooooo
+                                        double rate = 0;
+                                        if (line.trim().startsWith("RATE ")) {
+                                            rate = Double.parseDouble(line.trim().substring(5).trim());
+                                        }
+                                        expenseType = new ExpenseType2(id, desc, max, rate);
+                                        expenseTypes.add(expenseType);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error reading line");
         }
     }
 
